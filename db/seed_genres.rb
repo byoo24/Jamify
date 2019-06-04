36736d13da73b9f05358e1c3d5ab51b121d170a4
byoo24 <byoo.dev@gmail.com>
2019-06-04 10:02:48 -0400

    fixed typo in credentials

--- db/seed_genres.rb
+++ db/seed_genres.rb
@@ -1,17 +1,17 @@
-Genre.destroy_all
+# Genre.destroy_all
 
-pop = Genre.create(category_name: "Pop")
-rb = Genre.create(category_name: "R&B")
-hiphop = Genre.create(category_name: "Hip Hop")
+# pop = Genre.create(category_name: "Pop")
+# rb = Genre.create(category_name: "R&B")
+# hiphop = Genre.create(category_name: "Hip Hop")
 
 
-pop_cover = open('https://jamify-data.s3.amazonaws.com/genre_covers/pop.jpg')
-pop.genre_cover.attach(io: pop_cover, filename:'genre_pop.jpg')
+# pop_cover = open('https://jamify-data.s3.amazonaws.com/genre_covers/pop.jpg')
+# pop.genre_cover.attach(io: pop_cover, filename:'genre_pop.jpg')
 
-rb_cover = open('https://jamify-data.s3.amazonaws.com/genre_covers/r%26b.jpg')
-rb.genre_cover.attach(io: rb_cover, filename:'genre_rb.jpg')
+# rb_cover = open('https://jamify-data.s3.amazonaws.com/genre_covers/r%26b.jpg')
+# rb.genre_cover.attach(io: rb_cover, filename:'genre_rb.jpg')
 
-hiphop_cover = open('https://jamify-data.s3.amazonaws.com/genre_covers/hip_hop.jpg')
-hiphop.genre_cover.attach(io: hiphop_cover, filename:'genre_hiphop.jpg')
+# hiphop_cover = open('https://jamify-data.s3.amazonaws.com/genre_covers/hip_hop.jpg')
+# hiphop.genre_cover.attach(io: hiphop_cover, filename:'genre_hiphop.jpg')
 
 
